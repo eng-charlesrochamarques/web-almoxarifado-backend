926d81a0ddcb5919df05b645ac2a753d85f8c3bc
Charles Marques <charlesrochamarques@gmail.com>
2026-08-09 05:22:58 -0300

    Add centralized error handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const itemsRouter = require("./routes/items");
+const errorHandler = require("./middlewares/error-handler");
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -10,10 +11,17 @@ mongoose.connect("mongodb://127.0.0.1:27017/web-almoxarifado");
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/items", itemsRouter);
+
 app.get("/", (req, res) => {
   res.send({ message: "Web Almoxarifado API" });
 });
+
+app.use((req, res) => {
+  res.status(404).send({ message: "Recurso solicitado nao encontrado" });
+});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
